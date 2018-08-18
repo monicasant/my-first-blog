ee0d402908af543f7f305449d9602122dedb955e
Monica <you@example.com>
2018-08-18 08:27:51 +0530

    My Django Girls app,first commit

---
+++ python_intro.py
@@ -0,0 +1,17 @@
+print("Hello Django girls !")
+if 2>3:
+    print('It works !')
+else:
+    print('Not true')
+def hi():
+    print("Hello")
+    print("How you doin?")
+hi()
+def hi(name):
+    print('Hi '+name+ '!')
+girls=['Monica','Rachel','Angela']
+for name in girls:
+        hi(name)
+        print('Next')
+for i in range(1,6):
+    print(i)
